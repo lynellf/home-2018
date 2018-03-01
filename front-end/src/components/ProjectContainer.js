892bbf1e6b6f4b49672a197863267852ee15bf3f
Ezell L. Frazier <lynellf@gmail.com>
2018-03-01 07:23:40 -0600

    add functions helper to front-end

--- front-end/src/components/ProjectContainer.js
+++ front-end/src/components/ProjectContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ProjectArticle from './ProjectArticle';
+import { resetClassNames, defaultClass, grouper } from './helpers/functions';
 
 export default class ProjectContainer extends Component {
   constructor(props) {
@@ -8,6 +9,7 @@ export default class ProjectContainer extends Component {
       entries: this.props.posts,
       projectList: [],
       classNames: [],
+      postClassNames: [],
       titleClassNames: [],
       controlsClassNames: [],
     };
@@ -18,11 +20,13 @@ export default class ProjectContainer extends Component {
   }
 
   showProject(index, id) {
-    let { classList, controlsList, titleList } = this.resetClassNames();
+    // const classList = defaultClass(this.state.classNames, 'project__article'),
+    //   controlsList = defaultClass(this.state.controlsClassNames, 'project__controls'),
+    //   titleList = defaultClass(this.state.titleClassNames, 'project__title-container');
 
-    classList[index] = 'project__article--active';
-    controlsList[index] = 'project__controls--active';
-    titleList[index] = 'project__title-container--active';
+    // classList[index] = 'project__article--active';
+    // controlsList[index] = 'project__controls--active';
+    // titleList[index] = 'project__title-container--active';
 
     this.setState({
       classNames: classList,
@@ -39,30 +43,16 @@ export default class ProjectContainer extends Component {
     }, 50);
   }
 
-  resetClassNames() {
-    let classList = this.state.classNames,
-      titleList = this.state.titleClassNames,
-      controlsList = this.state.controlsClassNames;
-    for (let i = 0; i < classList.length; i++) {
-      classList[i] = 'project__article';
-      controlsList[i] = 'project__controls';
-      titleList[i] = 'project__title-container';
-    }
-    return { classList, controlsList, titleList };
-  }
-
   closeProject() {
-    let { classList, controlsList, titleList } = this.resetClassNames();
-
     this.setState({
-      classNames: classList,
-      titleClassNames: titleList,
-      controlsClassNames: controlsList,
+      classNames: defaultClass(this.state.ClassNames, 'project__article'),
+      titleClassNames: defaultClass(this.state.titleClassNames, 'project__title-container'),
+      controlsClassNames: defaultClass(this.state.controlsClassNames, 'project__controls'),
     });
   }
 
   nextProject(index, nextId) {
-    let { classList, controlsList, titleList } = this.resetClassNames();
+    let { classList, controlsList, titleList } = resetClassNames();
 
     classList[index + 1] = 'project__article--active';
     controlsList[index + 1] = 'project__controls--active';
@@ -84,7 +74,7 @@ export default class ProjectContainer extends Component {
   }
 
   previousProject(index, prevId) {
-    let { classList, controlsList, titleList } = this.resetClassNames();
+    let { classList, controlsList, titleList } = resetClassNames();
 
     classList[index - 1] = 'project__article--active';
     controlsList[index - 1] = 'project__controls--active';
@@ -123,82 +113,86 @@ export default class ProjectContainer extends Component {
     });
   }
 
-  componentDidMount() {
+  componentWillMount() {
     this.setClasses();
   }
 
   render() {
     const entries = this.state.entries,
-    entryLength = entries.length,
-    entryList = this.state.entries
-      .filter(post => post.tags.length > 0)
-      .map((post, index) => (
-        <div className="project" key={post._id} id={post._id}>
-          <div className={this.state.titleClassNames[index]}>
-            <h2
-              className="project__title"
-              onClick={event => this.showProject(index, post._id)}
-            >
-              {post.title}
-            </h2>
-            <div className={this.state.controlsClassNames[index]}>
-              {index !== 0 && (
-                <span
-                  className="controls__back"
-                  onClick={event =>
-                    this.previousProject(index, entries[index - 1]._id)
-                  }
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                  >
-                    <path d="M10 20l3-3-7-7 7-7-3-3L0 10l10 10z" />
-                  </svg>
-                </span>
-              )}
-              {index !== entryLength - 1 && (
-                <span
-                  className="controls__forward"
-                  onClick={event =>
-                    this.nextProject(index, entries[index + 1]._id)
-                  }
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                  >
-                    <path d="M3 20l-3-3 7-7-7-7 3-3 10 10L3 20z" />
-                  </svg>
-                </span>
-              )}
-              <span
-                className="controls__close"
-                onClick={event => this.closeProject()}
+      entryLength = entries.length,
+      entryList = this.state.entries
+        .filter(post => post.tags.length > 0)
+        .map((post, index) => (
+          <div className="project" key={post._id} id={post._id}>
+            <div className={this.state.titleClassNames[index]}>
+              <h2
+                className="project__title"
+                onClick={event => this.showProject(index, post._id)}
               >
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24">
-                  <path d="M20.775 2.52a.81.81 0 0 1 0 1.14l-6.276 6.269a.81.81 0 0 0 0 1.141l6.273 6.271a.81.81 0 0 1 0 1.14l-2.285 2.277a.814.814 0 0 1-1.142 0l-6.271-6.271a.812.812 0 0 0-1.141 0l-6.276 6.267a.81.81 0 0 1-1.141 0l-2.282-2.28a.808.808 0 0 1 0-1.139l6.278-6.269a.81.81 0 0 0 0-1.14L.24 3.653a.81.81 0 0 1 0-1.14L2.524.235a.81.81 0 0 1 1.141.001l6.271 6.27a.808.808 0 0 0 1.141.001L17.353.24a.812.812 0 0 1 1.141 0l2.281 2.28z" />
-                </svg>
-              </span>
+                {post.title}
+              </h2>
+              <div className={this.state.controlsClassNames[index]}>
+                {index !== 0 && (
+                  <span
+                    className="controls__back"
+                    onClick={event =>
+                      this.previousProject(index, entries[index - 1]._id)
+                    }
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                    >
+                      <path d="M10 20l3-3-7-7 7-7-3-3L0 10l10 10z" />
+                    </svg>
+                  </span>
+                )}
+                {index !== entryLength - 1 && (
+                  <span
+                    className="controls__forward"
+                    onClick={event =>
+                      this.nextProject(index, entries[index + 1]._id)
+                    }
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                    >
+                      <path d="M3 20l-3-3 7-7-7-7 3-3 10 10L3 20z" />
+                    </svg>
+                  </span>
+                )}
+                <span
+                  className="controls__close"
+                  onClick={event => this.closeProject()}
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                  >
+                    <path d="M20.775 2.52a.81.81 0 0 1 0 1.14l-6.276 6.269a.81.81 0 0 0 0 1.141l6.273 6.271a.81.81 0 0 1 0 1.14l-2.285 2.277a.814.814 0 0 1-1.142 0l-6.271-6.271a.812.812 0 0 0-1.141 0l-6.276 6.267a.81.81 0 0 1-1.141 0l-2.282-2.28a.808.808 0 0 1 0-1.139l6.278-6.269a.81.81 0 0 0 0-1.14L.24 3.653a.81.81 0 0 1 0-1.14L2.524.235a.81.81 0 0 1 1.141.001l6.271 6.27a.808.808 0 0 0 1.141.001L17.353.24a.812.812 0 0 1 1.141 0l2.281 2.28z" />
+                  </svg>
+                </span>
+              </div>
             </div>
-          </div>
 
-          <article className={this.state.classNames[index]}>
-            <ProjectArticle
-              post={post.body}
-              images={post.images}
-              skills={post.tags}
-              title={post.title}
-              date={post.date}
-              update={post.lastUpdated}
-              git={post.gitHub}
-              url={post.projectUrl}
-            />
-          </article>
-        </div>
-      ));
+            <article className={this.state.classNames[index]}>
+              <ProjectArticle
+                post={post.body}
+                images={post.images}
+                skills={post.tags}
+                title={post.title}
+                date={post.date}
+                update={post.lastUpdated}
+                git={post.gitHub}
+                url={post.projectUrl}
+              />
+            </article>
+          </div>
+        ));
 
     return (
       <div className="project__list" id="projects">
---
+++ front-end/src/components/helpers/functions.js
@@ -0,0 +1,57 @@
+// Reset classes on project elements
+export const defaultClass = (array, defaultName) => {
+    const newArray = [];
+    array.forEach(item => {
+      newArray.push(defaultName);
+    });
+
+    return newArray;
+  },
+  toggleElement = (array, index, defaultName) => {
+    array = defaultClass(array, defaultName);
+    array[index] = `${defaultName}--active`;
+    return array;
+  },
+  grouper = array => {
+    let groups = [],
+      group = [];
+    // We want to push 10 posts into the group array
+    for (let i = 0; i < array.length; i++) {
+      group.push(array[i]);
+      // When the group array length is equal to ten, we want to push it to the groups array and start over
+      if (group.length === 10) {
+        groups.push(group);
+        group = [];
+      }
+    }
+    // Whatever remains in the group array shall get pushed into the groups array and return the array
+    groups.push(group);
+    return groups;
+  },
+  // We input a nested array (array of arrays) and return an array with all but
+  // the first array with a 'disabled or hidden' className
+  filterElements = (elementArray, classArray, elementName) => {
+    let classNames = classArray;
+    if (elementArray[0].length >= 10) {
+      elementArray.forEach((item, index) => {
+        classNames[index].forEach((name, cIndex) => {
+          classNames[index][cIndex] = `${elementName}--inactive`;
+        });
+      });
+    } else if (elementArray[0].length > 0) {
+      classNames[0].forEach((className, index) => {
+        classNames[0][index] = 'project--inactive';
+      });
+
+      for (let i = 0; i < elementArray.length; i++) {
+        classNames[i].forEach((className, index) => {
+          classNames[i][index] = 'project--active';
+        });
+      }
+    } else {
+      classNames[0].forEach((className, index) => {
+        classNames[0][index] = 'project--inactive';
+      });
+    }
+    return classNames;
+  };
